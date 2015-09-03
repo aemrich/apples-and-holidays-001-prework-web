7e3ad0b7a56bf11b8fe27ffcaa6085a53980c50b
aemrich <adam.emrich@gmail.com>
2015-09-02 20:49:23 -0400

    halfway there - returning nil as 2nd element in fruit list with collect

--- lib/apple.rb
+++ lib/apple.rb
@@ -1,12 +1,23 @@
+#require 'pry'
+
 def apple_picker_with_select(fruit_list)
   # fruit_list will look something like: ["apple", "banana", "apple"]
+  fruit_list.select {|fruit| fruit =="apple"}
   # you want to return an array that just has apples in it
   
 end
 
 def apple_picker_with_collect(fruit_list)
+  new_list = []
   # fruit_list will look something like: ["apple", "banana", "apple"]
   # you want to return an array that just the apples, just like the 
   # challenge above
-  
+  #binding.pry
+    fruit_list.collect! do |n|
+      if n == "apple"
+        new_list << n
+      end
+    end
+    new_list
 end
+
